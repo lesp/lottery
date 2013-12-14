914de8300628f23cb6a8650eae92f0d6b5c53e27
Les Pounder <lespounder@gmail.com>
2013-12-14 15:09:02 +0000

    Added comments to my code, like every good programmer should

--- lottery_rand.py
+++ lottery_rand.py
@@ -1,16 +1,19 @@
 from random import *
 import os
 import time
+#Below is an empty list called numbers
 numbers = []
+#We now use a for loop to create the numbers 1 to 49 in our list
 for i in range(1,50):
     numbers.append(int(i))
 
 #os.system('mpg123 lottery.mp3 &')
-#Removed this due to copyright issues, but you can get the audio from the net, just google it :)
-time.sleep(5)
+#time.sleep(5)
 print "What I believe are the winning lottery numbers for tonight are..."
 time.sleep(3)
+#Now we shuffle the numbers stored in the list called "numbers"
 shuffle(numbers)
+#We slice out the first 7 numbers from the list
 a = numbers[0]
 b = numbers[1]
 c = numbers[2]
